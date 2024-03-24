986b011142893a0f6dd9d2e74763e196dec3ce4c
muffindud <ccatlabuga2002@gmail.com>
2024-03-24 19:25:41 +0200

    add task1

--- lab2/task1.py
+++ lab2/task1.py
@@ -7,23 +7,23 @@ def main():
     a = [-2, 0, 1, -1, 3]
     b = [1, 2, 0, -1]
     d = 5
-    n = [i for i in range(1, 1, d)]
+    n = [i for i in range(1, d + 1)]
     c = 4
-    l = [i for i in range(1, 1, c)]
+    l = [i for i in range(1, c + 1)]
 
-    plt.subplot(2, 1, 1)
     plt.stem(n, a)
     plt.title('Sequence a')
     plt.xlabel('Time index n')
     plt.ylabel('Amplitude')
+    plt.savefig('plots/task1a.png')
 
-    plt.subplot(2, 1, 2)
+    plt.clf()
+
     plt.stem(l, b)
     plt.xlabel('Time index n')
     plt.ylabel('Amplitude')
     plt.title('Sequence b')
-
-    plt.savefig('plots/task1.png')
+    plt.savefig('plots/task1b.png')
 
 
 
