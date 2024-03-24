5be73c589ee325c365803f0bc1408a3b8326110f
muffindud <ccatlabuga2002@gmail.com>
2024-03-24 19:49:05 +0200

    add task 2

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,10 +1,21 @@
 #!./venv/bin/python3
 
 import matplotlib.pyplot as plt
-
+from scipy.signal import convolve
 
 def main():
-    ...
+    a = [-2, 0, 1, -1, 3]
+    b = [1, 2, 0, -1]
+    d = 5
+    c = convolve(a, b)
+    k = [i for i in range(1, 8 + 1)]
+
+    plt.stem(k, c)
+    plt.title('a and b convolved')
+    plt.xlabel('Time index')
+    plt.ylabel('Amplitude')
+    plt.savefig('plots/task2.png')
+
 
 
 if __name__ == "__main__":
