60a3024e21b3ef9994f3932704004e93d5baa65d
muffindud <ccatlabuga2002@gmail.com>
2024-03-25 12:37:04 +0200

    add task4

--- lab2/task4.py
+++ lab2/task4.py
@@ -1,10 +1,24 @@
 #!./venv/bin/python3
 
 import matplotlib.pyplot as plt
+from numpy.fft import fft, ifft
 
 
 def main():
-    ...
+    a = [-2, 0, 1, -1, 3]
+    b = [1, 2, 0, -1]
+    m = 8
+    k = [i for i in range(1, m + 1)]
+    AE = fft(a, m)
+    BE = fft(b, m)
+    p = AE * BE
+    y1 = ifft(p, m)
+
+    plt.stem(k, y1)
+    plt.xlabel('Index')
+    plt.ylabel('Amplitude')
+    plt.title('IFFT of a * b')
+    plt.savefig('plots/task4.png')
 
 
 if __name__ == "__main__":
