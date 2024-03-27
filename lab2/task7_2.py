fcb2b7263824779d64441c263a05d5c306e08d9c
muffindud <ccatlabuga2002@gmail.com>
2024-03-27 11:18:47 +0200

    add task7

---
+++ lab2/task7_2.py
@@ -0,0 +1,30 @@
+#!./venv/bin/python3
+
+import matplotlib.pyplot as plt
+from numpy import pi
+from numpy.fft import fft, ifft
+from scipy.signal import sawtooth, square, convolve
+from timeit import default_timer
+
+
+def main():
+    d = 262144
+    n = [i for i in range(1, d + 1)]
+    a = 2.6 * square([28 * pi * i + 1 for i in n])
+    
+    c = 262144
+    l = [i for i in range(1, c + 1)]
+    b = 3.8 * sawtooth([22 * pi * i + 1 for i in l])
+
+    m = d + c
+    k = [i for i in range(1, m + 1)]
+
+    start = default_timer()
+    c = convolve(a, b)
+    end = default_timer()
+
+    print(f"Convolution time: {end - start}")
+
+
+if __name__ == "__main__":
+    main()
