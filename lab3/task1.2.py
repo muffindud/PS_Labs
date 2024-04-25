653df6ef9dd33c955690f69a4536b5b35921131c
muffindud <ccatlabuga2002@gmail.com>
2024-04-25 09:30:21 +0300

    task1.2

--- lab3/task1.2.py
+++ lab3/task1.2.py
@@ -1,9 +1,22 @@
 #!venv/bin/python3
 
 import matplotlib.pyplot as plt
+from math import sin, pi
+from scipy.fftpack import fft
 
 def main():
-    ...
+    N = 64
+    T = 1 / 128
+    k = [i for i in range(0, N - 1)]
+    f = [sin(2 * pi * 20 * i * T) for i in k]
+    F = fft(f)
+
+    plt.plot(k, abs(F))
+    plt.grid()
+    plt.xlabel('k')
+    plt.ylabel('Y(t)')
+    plt.title('FFT of the original signal')
+    plt.savefig('plots/task1.2.png')
 
 
 if __name__ == "__main__":
