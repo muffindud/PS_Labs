540b3ace91d3dbd2f8a48215b7c92b9e97c06dd9
muffindud <ccatlabuga2002@gmail.com>
2024-04-28 22:29:40 +0300

    task3.1

--- lab3/task3.1.py
+++ lab3/task3.1.py
@@ -1,9 +1,21 @@
 #!venv/bin/python3
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    ...
+    A = 0.75
+    w = 50
+    Ts = 0.01
+    T = 100
+    t = [i * Ts for i in range(0, int(T / Ts))]
+    x = A * np.where(np.abs(t) <= w / 2, 1, 0)
+
+    plt.stem(t, x)
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.title("Discrete signal")
+    plt.savefig("plots/task3.1.png")
 
 
 if __name__ == "__main__":
--- lab3/task3.2.py
+++ lab3/task3.2.py
@@ -1,9 +1,22 @@
 #!venv/bin/python3
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    ...
+    A = 0.75
+    w = 50
+    Ts = 0.01
+    T = 100
+    t = [i * Ts for i in range(0, int(T / Ts))]
+    x = A * np.where(np.abs(t) <= w / 2, 1, 0)
+    Fmax = 1 / Ts
+    f = [i * Fmax for i in range(0, Fmax)]
+    y = np.fft.fft(x)
+
+    plt.stem(t, np.abs(y))
+    
+    plt.savefig("plots/task3.1.png")
 
 
 if __name__ == "__main__":
