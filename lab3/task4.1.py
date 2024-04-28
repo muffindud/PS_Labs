7016e9cc0618e799493a64754336577f781f2dd2
muffindud <ccatlabuga2002@gmail.com>
2024-04-28 23:36:57 +0300

    task4.1

--- lab3/task4.1.py
+++ lab3/task4.1.py
@@ -1,9 +1,20 @@
 #!venv/bin/python3
 
 import matplotlib.pyplot as plt
+import random
 
 def main():
-    ...
+    Ts = 0.01
+    T = 50
+    t = [i * Ts for i in range(0, int(T / Ts))]
+    x1 = [random.random() for _ in range(len(t))]
+
+    plt.plot(t, x1, linewidth=0.1)
+    plt.grid()
+    plt.xlabel("Time")
+    plt.ylabel("Frequency")
+    plt.title("White noise signal")
+    plt.savefig("plots/task4.1.png")
 
 
 if __name__ == "__main__":
