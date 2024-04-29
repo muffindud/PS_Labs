8b8437218b1c4f8a0587e3ebd1eee9517499a2d8
muffindud <ccatlabuga2002@gmail.com>
2024-04-29 13:53:32 +0300

    task4.3

--- lab3/task4.3.py
+++ lab3/task4.3.py
@@ -20,18 +20,25 @@ def main():
     b = [A * 2 * dz * oms ** 2]
     y1 = lfilter(b, a, x1)
     df = 1 / T
-    Fmax = 1 / (2 * Ts)
+    Fmax = 1 / Ts
     f = [-Fmax / 2 + i * df for i in range(0, int(Fmax / df))]
     Fu1 = fft(x1)
     Fu2 = fft(y1)
     Fu1p = fftshift(Fu1)
     Fu2p = fftshift(Fu2)
 
+    plt.subplot(1, 2, 1)
     plt.stem(f, abs(Fu1p), markerfmt='r ', linefmt='r')
+    plt.xlabel("Frequency")
+    plt.ylabel("Magnitude")
+    plt.title("Input WN")
+
+    plt.subplot(1, 2, 2)
     plt.stem(f, abs(Fu2p), markerfmt='b ', linefmt='b')
     plt.xlabel("Frequency")
     plt.ylabel("Magnitude")
-    plt.title("White noise signal and its filtered version")
+    plt.title("Output WN")
+
     plt.savefig("plots/task4.3.png")
 
 
